cf9d76a65f5154b69f80365df02f34f9e54c2e34
Chance Tarver <tarver.chance@gmail.com>
2021-12-29 20:03:47 -0600

    Fill in main run function

--- src/MIMOSA.m
+++ src/MIMOSA.m
@@ -1,13 +1,15 @@
-classdef MIMOSA
-    %MIMOSA 
+classdef MIMOSA < handle
+    %MIMOSA
     
     properties
         p
+        timestamp
+        
+        % Main objects
         precoder
         dpd
         pa
         channel
-        
         
         % Signals at various points in the dataflow.
         v0_s        % Data we want to send to the UE.
@@ -20,21 +22,27 @@ classdef MIMOSA
     methods
         function obj = MIMOSA(p)
             obj.p = p;  % Save a copy of the params struct.
-           
+            
         end
         
         function run(obj)
             %run.
+            
+            obj.v0_s = Signal.create_ofdm();
+            obj.v1_precoded = obj.precoder.use(obj.v0_s);
+            obj.v2_dpd = obj.dpd.use(obj.v1_precoded);
+            obj.v3_pa = obj.pa.use(obj.v2_dpd);
+            obj.v4_ue_y = obj.channel.use(obj.v3_pa);
         end
         
         function cleanup(obj)
-            % Save the workspace. 
-            % Save the plots. 
+            % Save the workspace.
+            % Save the plots.
         end
         
         function plot(obj)
-           %plot. Run standard plots.
-           % Creates the beamgrid. 
+            %plot. Run standard plots.
+            % Creates the beamgrid.
         end
     end
 end
